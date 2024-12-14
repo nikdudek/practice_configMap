8ff1d446abfbbd5ef85f52972923e0f26361eaab
nikdudek <nik.dudek@gmail.com>
2024-12-14 20:06:54 +0100

    Added new class for the second tast in the IO Streams topic

---
+++ src/practice_configMap/SearchFilesByExtension.java
@@ -0,0 +1,10 @@
+package practice_configMap;
+
+import java.nio.file.Path;
+
+public class SearchFilesByExtension {
+	public long getNumberOfFilesWithExtension(Path pathToStartSearch, String extension) {
+		return 0;
+		//<write your code here>
+	}
+}

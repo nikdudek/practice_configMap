47205d20ca39ce2f61133cbbe0abc2db10523322
nikdudek <nik.dudek@gmail.com>
2024-12-14 20:43:46 +0100

    Finished with the second task

--- src/practice_configMap/SearchFilesByExtension.java
+++ src/practice_configMap/SearchFilesByExtension.java
@@ -1,10 +1,25 @@
 package practice_configMap;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 public class SearchFilesByExtension {
 	public long getNumberOfFilesWithExtension(Path pathToStartSearch, String extension) {
-		return 0;
-		//<write your code here>
+		long filesCounter = 0;
+		if (pathToStartSearch == null || extension == null || extension.isEmpty() ) {
+			return filesCounter;
+		}
+		try (Stream<Path> stream = Files
+				.find(pathToStartSearch, Integer.MAX_VALUE, (specifiedPath, attr) -> String.valueOf(specifiedPath)
+						.endsWith(extension))) {
+			filesCounter = stream
+					.count();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return filesCounter;
 	}
 }

ff0596800019db58f00cd732e50abcfd03d77514
nikdudek <nik.dudek@gmail.com>
2024-12-14 19:58:03 +0100

    Added an extra method for finding files with given specification (uses Streams)

--- src/practice_configMap/Main.java
+++ src/practice_configMap/Main.java
@@ -4,7 +4,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -32,11 +36,13 @@ public class Main {
 			System.out.println(e);
 		}
 		
+		findFilesWithJava(new String("src"), 2);
+		
 //		Path path = Path.of(filePath);
 //		System.out.println(ConfigMapHw.getValueFromConfigMap(path, directoryName).toString());
 	}
 	
-	private static void writeFileToPathFileOutputStream(String path, String textToWrite) throws FileNotFoundException, IOException{
+	private static void writeFileToPathFileOutputStream(String path, String textToWrite) throws FileNotFoundException, IOException {
 		FileOutputStream fos = null;
 		try {
 			fos = new FileOutputStream(path);
@@ -45,6 +51,21 @@ public class Main {
 		} finally {
 			if (fos != null) {
 				fos.close();
+			}
+		}
+	}
+	
+	private static void findFilesWithJava(String path, int maxDepth) throws IOException {
+		Path start = Paths.get(path);
+		try (Stream<Path> stream = Files
+				.find(start, maxDepth, (specificPath, attr) -> String.valueOf(specificPath)
+						.endsWith(".java"))) {
+			String joined = stream
+					.sorted()
+					.map(String::valueOf)
+					.collect(Collectors.joining(";"));
+			if (joined != null && !joined.isEmpty() ) {
+				System.out.println("Found: " + joined);
 			}
 		}
 	}

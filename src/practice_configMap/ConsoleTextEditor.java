b05a6ea3eba76940914e8ad9f9d983173e3e8685
nikdudek <nik.dudek@gmail.com>
2024-12-15 13:00:59 +0100

    Finished with task 3

--- src/practice_configMap/ConsoleTextEditor.java
+++ src/practice_configMap/ConsoleTextEditor.java
@@ -1,7 +1,40 @@
 package practice_configMap;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
+
 public class ConsoleTextEditor {
+	
+	private static final String EXIT_TEXT = "exit";
+	private File fileToWrite;
+	
+	public ConsoleTextEditor(File fileToWrite) {
+		this.fileToWrite = fileToWrite;
+	}
+	
+	private void saveTextToFile(StringBuilder sb) {
+		try {
+			Files.write(fileToWrite.toPath(), sb.toString().getBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public void start() {
-		//<write your code here>
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		
+		while (true) {
+			String userInput = sc.next();
+			if (userInput.equalsIgnoreCase(EXIT_TEXT)) {
+				saveTextToFile(sb);
+				return;
+			}
+			sb.append(userInput).append(System.lineSeparator());
+		}
+		
 	}
 }
